82ea5b6f44630163fe59f4884d1067384e20e089
vishalbanderwale <vishalsonu945@gmail.com>
2022-12-21 00:54:17 +0530

    addded css and js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-var username=prompt("give me username")
-var welcome="it works!!"+username;
-alert(welcome);
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(input)
+{
+    return serverURL  + "?" + "text=" + input;
+}
+function errorHandler(error)
+{
+    console.log("error occured",error);
+    alert("something wrong with server !try again after sometime ")
+
+}
+function clickHandler(){
+    var inputText=txtInput.Value;
+
+    fetch(getTranslateURL(inputText))                                                                                 (inputText))
+    .then(response=> response.json())
+    .then(json=> {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=TranslatedText;
+    })
+    .catch(errorHandler)
+};
+btnTranslate.addEventListener("click",clickHandler)
+
