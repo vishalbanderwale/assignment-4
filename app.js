2e9a60b90a4a1496088d3fef840662e92e96fd64
vishalbanderwale <vishalsonu945@gmail.com>
2022-12-21 01:45:15 +0530

    updated css

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
-var serverURL="https://api.funtranslations.com/translate/minion.json";
-function getTranslationURL(input)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text)
 {
-    return serverURL  + "?" + "text=" + input;
+    return serverURL + "?" + "text=" + text
 }
+
 function errorHandler(error)
 {
-    console.log("error occured",error);
-    alert("something wrong with server !try again after sometime ")
-
+    console.log("Error occured", error);
+    alert("Something wrong with server, try again after sometime");
 }
-function clickHandler(){
-    var inputText=txtInput.Value;
 
-    fetch(getTranslateURL(inputText))                                                                                 (inputText))
-    .then(response=> response.json())
-    .then(json=> {
-        var translatedText=json.contents.translated;
-        outputDiv.innerText=TranslatedText;
-    })
-    .catch(errorHandler)
+
+
+function clickHandler() {
+    // console.log("clicked");
+    // console.log("input", txtInput.value);
+    
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+        .catch(errorHandler)
+    
 };
-btnTranslate.addEventListener("click",clickHandler)
+
+btnTranslate.addEventListener("click", clickHandler)
 
